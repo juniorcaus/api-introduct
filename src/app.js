7fab637bec77ff4beb3d902e16ea8560eec449a5
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-11-01 17:26:11 -0300

    usando Insomnia e criando metado Post

--- src/app.js
+++ src/app.js
@@ -4,6 +4,20 @@ const app = express()
 
 app.use(express.json());
 
+app.get('/hello_world', (req, res) => {
+    res.send('teste rota get')
+})
+
+app.post('/create', (req, res) => {
+
+    const title = req.body.title
+
+    
+
+    res.send(`Titulo: ${title}`)
+})
+
+
 app.listen(5000, () => {
     console.log('Server running on port:' + 5000 )        
 })
